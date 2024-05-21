7663fe097e21ca26c2daecf7da04d24db687cd3a
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-05-20 21:43:37 -0400

    se agrega modelo orden de compra

--- app/models.py
+++ app/models.py
@@ -10,3 +10,22 @@ class producto(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class orden_compra(models.Model):
+    codigo_orden = models.AutoField(primary_key=True)
+
+    nombre_proveedor = models.CharField(max_length=70)
+    rut_proveedor = models.CharField(max_length=9)
+    direccion_empresa = models.CharField(max_length=70)
+    telefono_proveedor= models.CharField(max_length=70)
+    correo_empresa = models.EmailField
+    
+    nombre_cliente = models.CharField(max_length=70)
+    rut_cliente = models.CharField(max_length=9)
+    direccion_cliente = models.CharField(max_length=70)
+    telefono_cliente = models.CharField(max_length=70)
+    correo_cliente = models.EmailField
+
+    def __str__(self):
+            return self.nombre_producto
+    
