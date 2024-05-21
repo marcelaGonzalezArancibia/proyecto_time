2f2feab25114013ba812f97950ebc9f89f219122
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-05-20 21:13:39 -0400

    se agrega modelo del producto

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class producto(models.Model):
+    idproducto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=70)
+    marca = models.CharField(max_length=70)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
