f12567ef7dd45788ee5d1ded64f8450c71b18072
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-05-20 21:44:22 -0400

    se agregar orden de compra a admin

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import producto
+from .models import producto,orden_compra
 # Register your models here.
 admin.site.register(producto)
+admin.site.register(orden_compra)
