bc46b231ac3b88b2a2ba10fe21e1a7e61a9ecff9
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-05-20 21:01:24 -0400

    se agregan rutas pagina de productos

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
-from .views import index
+from .views import index,page_producto
+
 urlpatterns = [
     path('', index,name='index'),
+    path('page_producto',page_producto, name='page_producto')
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def page_producto(request):
+    return render(request,'producto.html')
