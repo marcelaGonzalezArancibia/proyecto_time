e4a6d129f2077d08c5fae7ff9fca027b4db43561
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-06-04 00:11:17 -0400

    el login esta  funcionando

--- proyectoreloj/settings.py
+++ proyectoreloj/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
