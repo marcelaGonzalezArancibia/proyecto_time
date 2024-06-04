c1b7c93e5b49347945f31c61045c5e86d1214687
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-06-03 22:53:37 -0400

    se crea login y su funcionalidad

--- proyectoreloj/settings.py
+++ proyectoreloj/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
