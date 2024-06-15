7ce7e716b1ea8e25d000515eb109140e2e282113
marcelaGonzalezArancibia <marce.gonzaleza@duocuc.cl>
2024-06-15 01:46:26 -0400

    modificar form de entrega

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import index,crearorden,listadoorden,detalleorden,modificar,entrega
+from .views import index,crearorden,listadoorden,detalleorden,modificar,entrega,DetalleOrdenPDF
 
 urlpatterns = [
     path('', index,name='index'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('listadoorden',listadoorden,name='listadoorden'),
     path('detalleorden/<int:orden_id>/',detalleorden,name='detalleorden'),
     path('modificar/<int:orden_id>/',modificar,name='modificar'),
-    path('entrega/<int:orden_id>/',entrega,name='entrega')
+    path('entrega/<int:orden_id>/',entrega,name='entrega'),
+    path('DetalleOrdenPDF/<int:orden_id>/',DetalleOrdenPDF.as_view(), name='DetalleOrdenPDF'),
 ]
